2e8c4efc71c79a7468c2a7ffe46ad5bd1764e000
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-09-08 14:02:11 -0300

    Aula 03.02 - CSS in JSX

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,24 @@
 import Link from '../src/components/Link'
 
+function Title({ children, as }) {
+  const Tag = as
+  return (
+    <>
+      <Tag>{children}</Tag>
+      <style jsx>{`
+        ${Tag} {
+          color: red;
+          font-family: sans-serif;
+        }
+      `}</style>
+    </>
+  )
+}
+
 export default function Home() {
   return (
     <>
-      <h1>Home Page</h1>
+      <Title as="h1">Alura Cases</Title>
       <p>
         Ir para o <Link href="/faq">FAQ</Link>
       </p>
