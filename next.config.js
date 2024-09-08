8499cab04f88aacab5da02ae1f66bee1259b7d76
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-09-08 16:39:37 -0300

    Aula 05.02 - netx.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 // Não é necessário para a versão 12.0.4
 
+const { redirect } = require('next/dist/server/api-utils')
+
 // /**
 //  * @type {import('next').NextConfig}
 //  */
@@ -7,4 +9,16 @@
 //   output: 'export',
 // }
 
-// module.exports = nextConfig
+module.exports = {
+  //   nextConfig,
+  trailingSlash: true,
+  async redirects() {
+    return [
+      {
+        source: '/perguntas',
+        destination: '/faq',
+        permanent: true,
+      },
+    ]
+  },
+}
