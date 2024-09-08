d5661d3706e8dbe6412805e837abeb4b6991d568
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-09-08 14:32:33 -0300

    Aula 03.05 - Componentes do Layout

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,3 @@
-import Link from '../src/components/Link'
+import HomeScreen from '../src/screens/HomeScreen'
 
-function Title({ children, as }) {
-  const Tag = as
-  return (
-    <>
-      <Tag>{children}</Tag>
-      <style jsx>{`
-        ${Tag} {
-          color: red;
-        }
-      `}</style>
-    </>
-  )
-}
-
-export default function Home() {
-  return (
-    <>
-      <Title as="h1">Alura Cases</Title>
-      <p>
-        Ir para o <Link href="/faq">FAQ</Link>
-      </p>
-    </>
-  )
-}
+export default HomeScreen
