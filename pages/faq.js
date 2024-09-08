f8f416e60f293173744bcce0bef253a9db0ee425
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-09-08 16:48:09 -0300

    Aula 05.03 - FAQ Screen

--- pages/faq.js
+++ pages/faq.js
@@ -1,18 +1,8 @@
-import Link from '../src/components/Link'
-import PageTitle from '../src/components/PageTitle'
+import FAQScreen from '../src/screens/FAQScreen'
 
-/** SSG - Static Site Generation */
-/** SSR - Server Side Rendering */
-/** ISG - Incremental Static Generation */
-
-//** export async function getServerSideProps() { */
-//** Roda sempre que a página receber um acesso */
-/** Em modo DEV roda a cada acesso */
+export default FAQScreen
 
 export async function getStaticProps() {
-  /** Roda SOMENTE em build time */
-  /** Em modo DEV roda a cada acesso */
-
   const FAQ_API_URL =
     'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
   const faq = await fetch(FAQ_API_URL)
@@ -28,24 +18,4 @@ export async function getStaticProps() {
       faq,
     },
   }
-}
-
-export default function Faq({ faq }) {
-  return (
-    <>
-      <PageTitle>FAQ - Alura Cases Campanha</PageTitle>
-      <h1>FAQ Page</h1>
-      <p>
-        Voltar para a <Link href="/">Home</Link>
-      </p>
-      <ul>
-        {faq.map(({ answer, question }) => (
-          <li key={question}>
-            <h2>{question}</h2>
-            <p>{answer}</p>
-          </li>
-        ))}
-      </ul>
-    </>
-  )
 }
