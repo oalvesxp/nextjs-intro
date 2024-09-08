fbaed98dc58bb7905d46eb84bafc2e0b6899f97f
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-09-08 16:01:42 -0300

    Aula 04.04  - getServerSideProps

--- pages/faq.js
+++ pages/faq.js
@@ -1,7 +1,17 @@
-import { useEffect, useState } from 'react'
 import Link from '../src/components/Link'
 
+/** SSG - Static Site Generation */
+/** SSR - Server Side Rendering */
+/** ISG - Incremental Static Generation */
+
+//** export async function getServerSideProps() { */
+//** Roda sempre que a página receber um acesso */
+/** Em modo DEV roda a cada acesso */
+
 export async function getStaticProps() {
+  /** Roda SOMENTE em build time */
+  /** Em modo DEV roda a cada acesso */
+
   const FAQ_API_URL =
     'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
   const faq = await fetch(FAQ_API_URL)
@@ -20,21 +30,6 @@ export async function getStaticProps() {
 }
 
 export default function Faq({ faq }) {
-  // const [faq, setFaq] = useState([])
-
-  // useEffect(() => {
-  //   const FAQ_API_URL =
-  //     'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
-  //   fetch(FAQ_API_URL)
-  //     .then((responseAPI) => {
-  //       return responseAPI.json()
-  //     })
-  //     .then((response) => {
-  //       setFaq(response)
-  //       console.log(response)
-  //     })
-  // }, [])
-
   return (
     <>
       <h1>FAQ Page</h1>
